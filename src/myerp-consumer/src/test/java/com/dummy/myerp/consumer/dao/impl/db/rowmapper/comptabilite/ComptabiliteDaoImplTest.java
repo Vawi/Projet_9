fbf6e7811ad71b19dbe8dcc6207fb05e07ea0377
Vawi <val.winnen@gmail.com>
2018-12-21 15:52:15 +0100

    Spring test consumer ok

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/ComptabiliteDaoImplTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/ComptabiliteDaoImplTest.java
@@ -1,5 +1,25 @@
 package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
 
+import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
+import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.List;
+
 public class ComptabiliteDaoImplTest {
+
+
+    @Test
+    public void getListJournalComptable() {
+
+        List<JournalComptable> vList = ComptabiliteDaoImpl.getInstance().getListJournalComptable();
+
+        System.out.println(vList);
+
+        Assert.assertTrue(vList.size() >= 1);
+
+    }
 
 }
--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/EcritureComptableRMTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/EcritureComptableRMTest.java
@@ -1,10 +1,6 @@
 package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
 
 
-import org.junit.Test;
-
-import java.sql.ResultSet;
-
 public class EcritureComptableRMTest {
 
 
