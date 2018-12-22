f0f8fe8f08ff3393ba9dc54ce8ee06035e933037
Vawi <val.winnen@gmail.com>
2018-12-22 10:46:06 +0100

    ecriture test insert, ne fonctionne pas, a corriger

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/ComptabiliteDaoImplTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/ComptabiliteDaoImplTest.java
@@ -4,12 +4,17 @@ import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.technical.exception.NotFoundException;
 import com.dummy.myerp.testconsumer.consumer.ConsumerTestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 public class ComptabiliteDaoImplTest extends ConsumerTestCase {
@@ -45,6 +50,28 @@ public class ComptabiliteDaoImplTest extends ConsumerTestCase {
     public void getEcritureComptableByRef() throws NotFoundException {
         EcritureComptable ec = dao.getEcritureComptableByRef("AC-2016/00001");
         Assert.assertNotNull(ec);
+    }
+
+    @Test
+    public void insertEcritureComptable() throws NotFoundException {
+        EcritureComptable ecriture  = new EcritureComptable();
+        Date currentDate = new Date();
+        int currentYear = LocalDateTime.ofInstant(currentDate.toInstant(), ZoneId.systemDefault()).toLocalDate().getYear();
+        ecriture.setJournal(new JournalComptable("OD", "TestInsert"));
+        ecriture.setReference("TT-" + currentYear + "/00200");
+        ecriture.setDate(currentDate);
+        ecriture.setLibelle("LibTest");
+
+        ecriture.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(111111),
+                "Test", new BigDecimal(200),new BigDecimal(200)));
+
+        dao.insertEcritureComptable(ecriture);
+        EcritureComptable ecritureBis = dao.getEcritureComptableByRef("TT-" + currentYear + "/00200");
+
+        Assert.assertTrue(ecriture.getReference().equals(ecritureBis.getReference()));
+        Assert.assertTrue(ecriture.getLibelle().equals(ecritureBis.getLibelle()));
+
+        dao.deleteEcritureComptable(ecritureBis.getId());
     }
 
 }
