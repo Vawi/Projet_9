95514db126e046d3b773d10460bb1d539f42ba45
Vawi <val.winnen@gmail.com>
2018-12-30 14:14:53 +0100

    Test rowmapper ok

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/EcritureComptableRMTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/EcritureComptableRMTest.java
@@ -1,8 +1,12 @@
 package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
 
 
+import com.dummy.myerp.consumer.ConsumerHelper;
+import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
+import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,6 +21,18 @@ public class EcritureComptableRMTest {
 
     @Mock
     private ResultSet resultSet;
+
+    @Mock
+    private static ComptabiliteDao comptabiliteDao;
+
+    @Mock
+    private DaoProxy daoProxy;
+
+    @Before
+    public void setUp(){
+        ConsumerHelper.configure(daoProxy);
+        Mockito.when(daoProxy.getComptabiliteDao()).thenReturn(comptabiliteDao);
+    }
 
     @Test
     public void mapRow() {
--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/JournalComptableRMTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/JournalComptableRMTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.validation.constraints.AssertTrue;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
