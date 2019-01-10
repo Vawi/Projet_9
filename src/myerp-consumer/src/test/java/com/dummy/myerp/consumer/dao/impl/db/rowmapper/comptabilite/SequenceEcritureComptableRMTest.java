567fad75807e963463d876db4f70352bace3d80a
Vawi <val.winnen@gmail.com>
2019-01-10 15:10:20 +0100

    correction test

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRMTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/rowmapper/comptabilite/SequenceEcritureComptableRMTest.java
@@ -1,16 +1,13 @@
 package com.dummy.myerp.consumer.dao.impl.db.rowmapper.comptabilite;
 
 import com.dummy.myerp.consumer.ConsumerHelper;
-import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
-import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.ResultSet;
@@ -23,15 +20,11 @@ public class SequenceEcritureComptableRMTest {
     private ResultSet resultSet;
 
     @Mock
-    private static ComptabiliteDao comptabiliteDao;
-
-    @Mock
     private DaoProxy daoProxy;
 
     @Before
     public void setUp(){
         ConsumerHelper.configure(daoProxy);
-        Mockito.when(daoProxy.getComptabiliteDao()).thenReturn(comptabiliteDao);
     }
 
     @Test
@@ -41,18 +34,12 @@ public class SequenceEcritureComptableRMTest {
         SequenceEcritureComptable seq = null;
 
         try {
-
-            Mockito.when(resultSet.getString("annee")).thenReturn("2018");
-            Mockito.when(resultSet.getString("derniere_valeur")).thenReturn("56");
-
             seq = rm.mapRow(resultSet, 0);
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         Assert.assertNotNull(seq);
-
 
     }
 
